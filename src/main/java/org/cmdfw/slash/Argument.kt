aa6ff092620f387ac2a95d3161f96f061767d726
AlvaroMS25 <alvaro2ms5@gmail.com>
2023-10-13 13:02:36 +0200

    use interface and implement autocomplete

--- src/main/java/org/cmdfw/slash/Argument.kt
+++ src/main/java/org/cmdfw/slash/Argument.kt
@@ -2,15 +2,21 @@ package org.cmdfw.slash
 
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.interactions.commands.OptionType
+import java.util.function.Function
 
 internal class Argument(
     private val parent: BuilderImpl
-) : SlashCommandArgument() {
+) : SlashCommandArgument {
     private lateinit var name: String
     private lateinit var description: String
     private lateinit var jda: JDA
     private lateinit var optionType: OptionType
     private val choices: MutableList<NameValue> = mutableListOf()
+    private var autocompleteProvider: Function<AutocompleteContext, Void>? = null
+    override fun autocomplete(provider: Function<AutocompleteContext, Void>?): SlashCommandArgument {
+        this.autocompleteProvider = provider
+        return this
+    }
 
     override fun setOptionType(t: OptionType): SlashCommandArgument {
         this.optionType = t
